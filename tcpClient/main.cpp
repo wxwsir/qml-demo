c9fc8b4eb82724ddbcc0c75cdc3cae3c4be5d975
wxwsir <wxw550w@163.com>
2024-07-30 09:24:00 +0800

    更改

--- tcpClient/main.cpp
+++ tcpClient/main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
     qmlRegisterType<NetWork>("NetWork",1,0,"NetWork");
     QQmlApplicationEngine engine;
-    const QUrl url(QStringLiteral("qrc:/untitled71/Main.qml"));
+    const QUrl url(QStringLiteral("qrc:/tcpClient/Main.qml"));
     QObject::connect(
         &engine,
         &QQmlApplicationEngine::objectCreationFailed,
